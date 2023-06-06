d0acd541bda44a53841a06ef98e6951330577c60
Sakura115 <s2123024@cco.kanagawa-it.ac.jp>
2023-06-07 02:09:50 +0900

    色々追加しました。 メインのプレイヤースクリプトを作りそこから継承して、力持ち、散弾銃、変化球(魔法使い)の三種類が作れるようにしました。 名前にtmpのついているシーンはプレイヤーの動作試験用に作ったものです。

--- Neko2D/Code/slipper_tmp.gd
+++ Neko2D/Code/slipper_tmp.gd
@@ -1,15 +1,15 @@
-extends Node
+extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var thrown_force = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func thrown(force:float,curve:bool):
+	thrown_force = force
+	print("thrown"+str(force)+"curve:"+str(curve))
+	return self
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
