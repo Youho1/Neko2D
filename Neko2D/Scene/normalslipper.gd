90722a5b2a2381e687b99141dfac200fcea99124
2123003ヨウホ <youho98zzz@gmail.com>
2023-06-07 14:05:32 +0900

    ヨウホ

---
+++ Neko2D/Scene/normalslipper.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	var mouse_position = get_global_mouse_position()
+	print("mouse_position")
+#	pass
