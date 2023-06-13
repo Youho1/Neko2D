30fc6a1b4fe919e717ee2f5de53f994fe11fef6a
2123089-PC\2123089 <s2123089@kanagawa-it.ac.jp>
2023-06-13 13:44:05 +0900

    一旦進捗を共有しておきます

--- Neko2D/Scene/light sripper.gd
+++ Neko2D/Scene/light sripper.gd
@@ -4,9 +4,8 @@ var screen_size
 func _ready():
 	screen_size = get_viewport_rect().size
 
-
 func _process(delta):
 	var velocity = Vector2()
-	velocity.x += 300
+	velocity.x += 400
 	position += velocity * delta
 
